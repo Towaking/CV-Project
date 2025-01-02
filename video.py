97eaac7c1abe24593f75c8534fdcc97fb1664713
TowaKing <goldwinho@gmail.com>
2025-01-02 21:48:19 +0700

    last commit fr

--- video.py
+++ video.py
@@ -5,44 +5,10 @@ from ultralytics import YOLO
 import easyocr
 from PIL import Image
 
-model = YOLO(r"./train48/weights/best.pt") 
+model = YOLO(r"./train48/weights/best.pt")
 reader = easyocr.Reader(['id'])
 
-def correct_image_rotation(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
-    
-    # if lines is not None:
-    #     angles = []
-    #     for rho, theta in lines[:, 0]:
-    #         angle = np.degrees(theta) - 90
-    #         angles.append(angle)
-        
-    #     median_angle = 90
-        
-    #     center = tuple(np.array(image.shape[1::-1]) / 2)  # Calculate the center of the image
-        
-    #     M = cv2.getRotationMatrix2D(center, -median_angle, 1.0)
-        
-    #     abs_cos = abs(M[0, 0])
-    #     abs_sin = abs(M[0, 1])
-        
-    #     bound_w = int(image.shape[1] * abs_cos + image.shape[0] * abs_sin)
-    #     bound_h = int(image.shape[1] * abs_sin + image.shape[0] * abs_cos)
-        
-    #     M[0, 2] += bound_w / 2 - center[0]
-    #     M[1, 2] += bound_h / 2 - center[1]
-        
-    #     rotated_image = cv2.warpAffine(image, M, (bound_w, bound_h), flags=cv2.INTER_CUBIC)
-        
-    #     return rotated_image
-    
-    return image
-
 def process_image(image, apply_rotation=True):
-    if apply_rotation:
-        image = correct_image_rotation(image)
     results = model(image)
 
     license_plate_label = "license_plate"  
@@ -108,7 +74,7 @@ def process_live_feed():
 st.title("License Plate Detection and Recognition")
 
 
-option = st.sidebar.selectbox("Choose Input Source", ("Image", "Video", "Live Feed"))
+option = st.sidebar.selectbox("Choose Input Source", ("Image"))
 
 if option == "Image":
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
@@ -118,14 +84,6 @@ if option == "Image":
         processed_image = process_image(image)
         st.image(processed_image, caption='Processed Image with License Plate Detection')
 
-elif option == "Video":
-    video_file = st.file_uploader("Upload a video", type=["mp4", "avi", "mov"])
-    if video_file is not None:
-        video_bytes = video_file.read()
-        video_path = f"temp_video.mp4"
-        with open(video_path, "wb") as f:
-            f.write(video_bytes)
-        process_video(video_path)
 
 elif option == "Live Feed":
     process_live_feed()
